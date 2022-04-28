f3e29353521215fef58b663e4fcb6b17e9fb0371
Rakesh Darnasi <r.darnasi@zensar.com>
2022-04-28 12:09:16 +0530

    add file

--- datetime.java
+++ datetime.java
@@ -3,7 +3,7 @@ import java.time.ZoneId;
 public class datetime {
     public static void main(String[] args) {
         // System.out.println(LocalTime.now());
-        // System.out.println(LocalDate.now());
+        // System.out.println(LocalDate.now())
 
         // System.out.println(LocalDateTime.now());
         // ZoneId.getAvailableZoneIds().forEach(System.out::println); // Collection
--- if_condition.java
+++ if_condition.java
@@ -1,4 +1,4 @@
-public class if_conditions {
+public class if_condition {
     public static void main(String[] args) {
         int value = 400;
 
